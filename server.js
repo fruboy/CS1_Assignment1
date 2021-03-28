012fc0ab076ef974b53663b702386c8425df554e
fruboy <frazhaider15@gmail.com>
2021-03-29 00:33:42 +0500

    third

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userModel = require("./userModel");
-
 const app = express();
-
 app.use(express.json());
 
 mongoose.connect(
